a69c1d2822bb0d7f3cb0551ae401e797bd94a449
bryanho0470 <bryanho0470@gmail.com>
2024-07-14 17:48:33 +0900

    test ignore?

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -6,9 +6,7 @@ import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
-///
-/// Example:
-/// ```dart
+
 /// import 'firebase_options.dart';
 /// // ...
 /// await Firebase.initializeApp(

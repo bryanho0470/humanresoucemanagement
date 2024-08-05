f505864c92f87b91c3a0009fd1f97a16d114ca86
bryanho0470 <bryanho0470@gmail.com>
2024-08-06 07:36:38 +0900

    how to set display character limit
    set differently list view and popup view

--- lib/pages/sharing_page.dart
+++ lib/pages/sharing_page.dart
@@ -1,8 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:humanresoucemanagement/global/common/hero_dialog_route.dart';
 import 'package:humanresoucemanagement/models/question_data_model.dart';
-import 'package:humanresoucemanagement/styles/custom_rect_tween.dart';
+import 'package:humanresoucemanagement/widgets/question_card_widget.dart';
 
 class SharingPage extends StatefulWidget {
   const SharingPage({super.key});
@@ -63,7 +62,7 @@ class _SharingPageState extends State<SharingPage> {
                           );
                         } else {
                           final questionData = snapshot.data!;
-                          return _QuestionContents(
+                          return QuestionContents(
                             questions: questionData,
                           );
                         }
@@ -76,206 +75,6 @@ class _SharingPageState extends State<SharingPage> {
           ),
         ),
       ),
-    );
-  }
-}
-
-class _QuestionContents extends StatelessWidget {
-  const _QuestionContents({
-    super.key,
-    required this.questions,
-  });
-
-  final List<Question> questions;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: questions.length,
-      itemBuilder: (context, index) {
-        final question = questions[index];
-        return _QuestionCard(question: question);
-      },
-    );
-  }
-}
-
-class _QuestionCard extends StatelessWidget {
-  const _QuestionCard({
-    super.key,
-    required this.question,
-  });
-
-  final Question question;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.of(context).push(HeroDialogRoute(
-          builder: (context) => Center(
-            child: _QuestionPopupCard(question: question),
-          ),
-        ));
-      },
-      child: Hero(
-        createRectTween: (begin, end) {
-          return CustomRectTween(begin: begin, end: end);
-        },
-        tag: question.id,
-        child: Padding(
-          padding: const EdgeInsets.all(12),
-          child: Material(
-            shadowColor: Colors.grey,
-            elevation: 2,
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(15),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  _QuestionTitle(title: question.title),
-                  ...[
-                    const Divider(),
-                    _QuestionItemBox(question: question),
-                  ],
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _QuestionTitle extends StatelessWidget {
-  const _QuestionTitle({
-    super.key,
-    required this.title,
-  });
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      title,
-      style: const TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 16,
-      ),
-    );
-  }
-}
-
-class _QuestionPopupCard extends StatelessWidget {
-  const _QuestionPopupCard({
-    super.key,
-    required this.question,
-  });
-
-  final Question question;
-
-  @override
-  Widget build(BuildContext context) {
-    return Hero(
-      tag: question.id,
-      createRectTween: (begin, end) {
-        return CustomRectTween(begin: begin, end: end);
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Material(
-          borderRadius: BorderRadius.circular(16),
-          color: Colors.white,
-          child: SizedBox(
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    _QuestionTitle(title: question.title),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    ...[
-                      const Divider(),
-                      _QuestionItemBox(question: question),
-                    ],
-                    Container(
-                      margin: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: const TextField(
-                        maxLines: 8,
-                        cursorColor: Colors.white,
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.all(8),
-                          hintText: "write a questions...",
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _QuestionItemBox extends StatelessWidget {
-  const _QuestionItemBox({
-    super.key,
-    required this.question,
-  });
-
-  final Question question;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: [
-      _QuestionItemTile(question: question),
-    ]);
-  }
-}
-
-class _QuestionItemTile extends StatefulWidget {
-  const _QuestionItemTile({
-    super.key,
-    required this.question,
-  });
-
-  final Question question;
-
-  @override
-  State<_QuestionItemTile> createState() => __QuestionItemTileState();
-}
-
-class __QuestionItemTileState extends State<_QuestionItemTile> {
-  bool val = false;
-  void _onChanged(bool? val) {
-    setState(() {
-      widget.question.completed = val!;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: Checkbox(
-        onChanged: _onChanged,
-        value: widget.question.completed,
-      ),
-      title: Text(widget.question.contents),
     );
   }
 }
--- lib/widgets/question_card_widget.dart
+++ lib/widgets/question_card_widget.dart
@@ -1,54 +1,266 @@
 import 'package:flutter/material.dart';
+import 'package:humanresoucemanagement/global/common/hero_dialog_route.dart';
+import 'package:humanresoucemanagement/models/question_data_model.dart';
+import 'package:humanresoucemanagement/styles/custom_rect_tween.dart';
 
-class QuestionCard extends StatelessWidget {
-  const QuestionCard({
+class QuestionContents extends StatelessWidget {
+  const QuestionContents({
     super.key,
+    required this.questions,
   });
+
+  final List<Question> questions;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 4.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: SizedBox(
-        width: double.infinity,
-        child: Column(
-          children: <Widget>[
-            const ClipRRect(
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(15),
+    return ListView.builder(
+      itemCount: questions.length,
+      itemBuilder: (context, index) {
+        final question = questions[index];
+        return _QuestionCard(question: question);
+      },
+    );
+  }
+}
+
+class _QuestionCard extends StatelessWidget {
+  const _QuestionCard({
+    required this.question,
+  });
+
+  final Question question;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(HeroDialogRoute(
+          builder: (context) => Center(
+            child: _QuestionPopupCard(question: question),
+          ),
+        ));
+      },
+      child: Hero(
+        createRectTween: (begin, end) {
+          return CustomRectTween(begin: begin, end: end);
+        },
+        tag: question.id,
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Material(
+            shadowColor: Colors.grey,
+            elevation: 2,
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  _QuestionTitle(title: question.title),
+                  ...[
+                    const Divider(),
+                    _QuestionItemBox(question: question),
+                  ],
+                ],
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.all(10),
-              child: Text(
-                "Xard title",
-                style: TextStyle(
-                  fontSize: 10,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "Button1",
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text("Button2"),
-                ),
-              ],
-            )
-          ],
+          ),
         ),
       ),
     );
+  }
+}
+
+///[=====================================================================]
+
+// sharing list title setting
+class _QuestionTitle extends StatelessWidget {
+  const _QuestionTitle({
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: const TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+      ),
+    );
+  }
+}
+
+///[=====================================================================]
+
+class _QuestionItemBox extends StatelessWidget {
+  const _QuestionItemBox({
+    required this.question,
+  });
+
+  final Question question;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      _QuestionItemTile(question: question),
+    ]);
+  }
+}
+
+///[=====================================================================]
+
+class _QuestionItemTile extends StatefulWidget {
+  const _QuestionItemTile({
+    required this.question,
+  });
+
+  final Question question;
+
+  @override
+  State<_QuestionItemTile> createState() => __QuestionItemTileState();
+}
+
+class __QuestionItemTileState extends State<_QuestionItemTile> {
+  bool val = false;
+  void _onChanged(bool? val) {
+    setState(() {
+      widget.question.completed = val!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // set chatacter limit
+    const int textLimit = 15;
+
+    // truncate the text if it exceeds the limit
+    String truncatedText = widget.question.contents.length > textLimit
+        ? '${widget.question.contents.substring(0, textLimit)}...'
+        : widget.question.contents;
+
+    return ListTile(
+      leading: Checkbox(
+        onChanged: _onChanged,
+        value: widget.question.completed,
+      ),
+      title: Text(truncatedText),
+    );
+  }
+}
+
+///[=====================================================================]
+
+class _QuestionPopupCard extends StatelessWidget {
+  const _QuestionPopupCard({
+    required this.question,
+  });
+
+  final Question question;
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: question.id,
+      createRectTween: (begin, end) {
+        return CustomRectTween(begin: begin, end: end);
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Material(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.white,
+          child: SizedBox(
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    _QuestionTitle(title: question.title),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    ...[
+                      const Divider(),
+                      _QuestionFullItemBox(question: question),
+                    ],
+                    Container(
+                      margin: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: const TextField(
+                        maxLines: 8,
+                        cursorColor: Colors.white,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(8),
+                          hintText: "write a questions...",
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+///[=====================================================================]
+
+class _QuestionFullItemTile extends StatefulWidget {
+  const _QuestionFullItemTile({
+    required this.question,
+  });
+
+  final Question question;
+
+  @override
+  State<_QuestionFullItemTile> createState() => __QuestionFullItemTileState();
+}
+
+class __QuestionFullItemTileState extends State<_QuestionFullItemTile> {
+  bool val = false;
+  void _onChanged(bool? val) {
+    setState(() {
+      widget.question.completed = val!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Checkbox(
+        onChanged: _onChanged,
+        value: widget.question.completed,
+      ),
+      title: Text(widget.question.contents),
+    );
+  }
+}
+
+///[=====================================================================]
+
+class _QuestionFullItemBox extends StatelessWidget {
+  const _QuestionFullItemBox({
+    required this.question,
+  });
+
+  final Question question;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      _QuestionFullItemTile(question: question),
+    ]);
   }
 }

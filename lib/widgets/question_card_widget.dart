601e73dfa4e7db6ff27b5073eaf9d34859103f41
bryanho0470 <bryanho0470@gmail.com>
2024-08-08 08:20:01 +0900

    making chip and wrap
    If your chips go over to outside of screen, you need to wrap with expanded widget.

--- lib/widgets/question_card_widget.dart
+++ lib/widgets/question_card_widget.dart
@@ -23,6 +23,8 @@ class QuestionContents extends StatelessWidget {
   }
 }
 
+///[=====================================================================]
+
 class _QuestionCard extends StatelessWidget {
   const _QuestionCard({
     required this.question,
@@ -84,12 +86,49 @@ class _QuestionTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      title,
-      style: const TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 20,
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 32,
+              child: Icon(
+                Icons.priority_high,
+                color: Colors.red,
+              ),
+            ),
+            SizedBox(
+              width: 32,
+              child: Icon(
+                Icons.flash_on_rounded,
+                color: Colors.red,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          width: 220,
+          child: Text(
+            title,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 65,
+          child: TextButton(
+            onPressed: () {},
+            child: const Text(
+              "Hide",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
@@ -142,12 +181,84 @@ class __QuestionItemTileState extends State<_QuestionItemTile> {
         ? '${widget.question.contents.substring(0, textLimit)}...'
         : widget.question.contents;
 
-    return ListTile(
-      leading: Checkbox(
-        onChanged: _onChanged,
-        value: widget.question.completed,
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                width: 130,
+                child: Text(truncatedText),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              const Expanded(
+                child: Wrap(
+                  children: [
+                    Chip(
+                      label: Text(
+                        'Economy',
+                        style: TextStyle(fontSize: 10),
+                      ),
+                      backgroundColor: Colors.blue,
+                      padding: EdgeInsets.all(1),
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      labelStyle: TextStyle(color: Colors.white),
+                    ),
+                    Chip(
+                      label: Text(
+                        "Social",
+                        style: TextStyle(fontSize: 10),
+                      ),
+                      backgroundColor: Colors.orange,
+                      padding: EdgeInsets.all(4),
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      labelStyle: TextStyle(color: Colors.white),
+                    ),
+                    Chip(
+                      label: Text(
+                        "Environment",
+                        style: TextStyle(fontSize: 10),
+                      ),
+                      backgroundColor: Colors.green,
+                      padding: EdgeInsets.all(4),
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      labelStyle: TextStyle(color: Colors.white),
+                    ),
+                    Chip(
+                      label: Text(
+                        "Sports",
+                        style: TextStyle(fontSize: 10),
+                      ),
+                      backgroundColor: Colors.purple,
+                      padding: EdgeInsets.all(4),
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      labelStyle: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            width: 350,
+            child: Text(
+              textAlign: TextAlign.right,
+              "Owned by ${widget.question.owner} : ${widget.question.name}",
+              style: const TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
+              ),
+            ),
+          )
+        ],
       ),
-      title: Text(truncatedText),
     );
   }
 }
@@ -188,22 +299,6 @@ class _QuestionPopupCard extends StatelessWidget {
                       const Divider(),
                       _QuestionFullItemBox(question: question),
                     ],
-                    Container(
-                      margin: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: const TextField(
-                        maxLines: 8,
-                        cursorColor: Colors.white,
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.all(8),
-                          hintText: "write a questions...",
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
                   ],
                 ),
               ),
@@ -212,6 +307,23 @@ class _QuestionPopupCard extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+///[=====================================================================]
+
+class _QuestionFullItemBox extends StatelessWidget {
+  const _QuestionFullItemBox({
+    required this.question,
+  });
+
+  final Question question;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      _QuestionFullItemTile(question: question),
+    ]);
   }
 }
 
@@ -238,29 +350,38 @@ class __QuestionFullItemTileState extends State<_QuestionFullItemTile> {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Checkbox(
-        onChanged: _onChanged,
-        value: widget.question.completed,
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.asset(
+                "assets/images/person/face.JPG",
+                width: 30,
+                height: 30,
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Text(widget.question.contents),
+            ],
+          ),
+          SizedBox(
+            width: 350,
+            child: Text(
+              textAlign: TextAlign.right,
+              "Owned by ${widget.question.owner} : ${widget.question.name}",
+              style: const TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
+              ),
+            ),
+          )
+        ],
       ),
-      title: Text(widget.question.contents),
     );
-  }
-}
-
-///[=====================================================================]
-
-class _QuestionFullItemBox extends StatelessWidget {
-  const _QuestionFullItemBox({
-    required this.question,
-  });
-
-  final Question question;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: [
-      _QuestionFullItemTile(question: question),
-    ]);
   }
 }
